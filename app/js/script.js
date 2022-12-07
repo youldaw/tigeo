b1b37641f1abc111b9ed25adb02f8ba1d05dbf9c
youldaw <68648932+youldaw@users.noreply.github.com>
2022-12-07 13:03:02 +0500

    update files
    up to date

--- app/js/script.js
+++ app/js/script.js
@@ -156,7 +156,85 @@ $(function (){
       e.preventDefault();
       $('.header').toggleClass('active');
     });
+
+    $('.ser-tab-btn a').on('click', function (e) {
+      e.preventDefault();
+      $('.service-price-tab').removeClass('hide');
+    });
+
+
+    $('.calc-next').on('click', function (e) {
+      e.preventDefault();
+      $('.step-one').removeClass('active');
+      $('.step-two').addClass('active');
+    });
+    $('.calc-back').on('click', function (e) {
+      e.preventDefault();
+      $('.step-two').removeClass('active');
+      $('.step-one').addClass('active');
+    });
+
+    $('.head-basket-btn').on('click', function (e) {
+      e.preventDefault();
+      $(this).parent().toggleClass('active');
+    });
+    
+    $(document).mouseup(function (e){
+      var div = $(".head-basket-block");
+      if (!div.is(e.target) 
+          && div.has(e.target).length === 0) {
+          div.removeClass('active');
+      }
+    });
+
+
+
+    $('select').each(function(){
+      var $this = $(this), numberOfOptions = $(this).children('option').length;
+    
+      $this.addClass('select-hidden'); 
+      $this.wrap('<div class="select"></div>');
+      $this.after('<div class="select-styled"></div>');
   
-  });
+      var $styledSelect = $this.next('div.select-styled');
+      $styledSelect.text($this.children('option').eq(0).text());
+    
+      var $list = $('<ul />', {
+          'class': 'select-options'
+      }).insertAfter($styledSelect);
+    
+      for (var i = 0; i < numberOfOptions; i++) {
+          $('<li />', {
+              text: $this.children('option').eq(i).text(),
+              rel: $this.children('option').eq(i).val()
+          }).appendTo($list);
+      }
+    
+      var $listItems = $list.children('li');
+    
+      $styledSelect.click(function(e) {
+          e.stopPropagation();
+          $('div.select-styled.active').not(this).each(function(){
+              $(this).removeClass('active').next('ul.select-options').hide();
+          });
+          $(this).toggleClass('active').next('ul.select-options').toggle();
+      });
+    
+      $listItems.click(function(e) {
+          e.stopPropagation();
+          $styledSelect.text($(this).text()).removeClass('active');
+          $this.val($(this).attr('rel'));
+          $list.hide();
+          //console.log($this.val());
+      });
+    
+      $(document).click(function() {
+          $styledSelect.removeClass('active');
+          $list.hide();
+      });
+  
+    });
+  
+});
   
   
